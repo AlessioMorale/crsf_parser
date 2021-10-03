a3ec8b3262f0afea191917882e03529c69b6710d
Alessio Morale <alessiomorale@gmail.com>
2021-10-03 18:28:58 +0100

    fix: cleanup example

--- example/build_frame.py
+++ example/build_frame.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from crsf_parser.payloads import PacketsTypes
 from crsf_parser import crsf_frame
-from crsf_parser.handling import crsf_build_frame, crsf_frame_crc
+from crsf_parser.handling import crsf_build_frame
 
 frame = crsf_build_frame(
     PacketsTypes.RC_CHANNELS_PACKED,
@@ -12,5 +12,3 @@ data = crsf_frame.parse(frame)
 print(crsf_frame.header.data_offset)
 print(data, type(data))
 print(frame, len(frame), type(frame))
-crc_test_frame = b"\xc8\x18\x16\xde\x03\x9f.\xb4\xf7\x0b\xf0\x81\x0f|\xe0\x03\x1f\xf8(\x08\x00\x00D<\xe2\xff"
-print(crc_test_frame)
